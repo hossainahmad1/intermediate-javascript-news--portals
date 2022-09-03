02a2a73fbc7c0fb8ac6e72976d176d0a6ae741cb
hossainahmad1 <108587702+hossainahmad1@users.noreply.github.com>
2022-09-03 21:26:05 +0600

    added spinner

--- index.js
+++ index.js
@@ -6,42 +6,32 @@ const loadNav = () => {
 }
 const displayNav = loads => {
     const dataContainer = document.getElementById('data-container');
-    
-    loads.forEach(load => {
 
-        console.log(load);
+    loads.forEach(load => {
+        toggleSpinner(true);
+        // console.log(load);
         const createLi = document.createElement('li');
         createLi.innerHTML = `
         <p onclick ="loadData('${load.category_id}')"> ${load.category_name}</p>
         `;
         dataContainer.appendChild(createLi);
     });
+
 }
-
-
-// const loadDataDetails = async (id) => {
-//     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-//     const res = await fetch(url)
-//     const data = await res.json()
-//     displayloadData(data.data);
-// }
-// const displayloadData = data => {
-//     console.log(data);
-// }
 
 
 // loadData display show
 const loadData = async (id) => {
-    const url = ` https://openapi.programming-hero.com/api/news/category/${id}`
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url)
     const data = await res.json()
     displayDataLoad(data.data);
 }
 const displayDataLoad = loadData => {
     const displayDataContainer = document.getElementById('display-data-container');
+    displayDataContainer.innerHTML = '';
     loadData.forEach(show => {
         console.log(show)
-
         const dataDiv = document.createElement('div');
         dataDiv.classList.add('row');
         dataDiv.innerHTML = `
@@ -54,24 +44,58 @@ const displayDataLoad = loadData => {
                             <p class="card-text text-muted">${show.details}.</p>
                             <div class = "d-flex">
                                 <div>
-                                <img src="${show.author.img}" class="img-fluid rounded" style="max-width: 50px;" alt="...">
+                                <img src="${show.author.img}" class="img-fluid rounded-circle" style="max-width: 50px;" alt="...">
                                 </div>
                                 <div>
-                                    <p>${show.author.name}</p>
+                                    <p>${show.author.name ? show.author.name : 'No Author Name'}</p>
                                     <p class= "text-muted">${show.author.published_date}</p>
                                 </div>
+                                <div>${show.total_view ? show.total_view : 'No View'}</div>
                                 <div>
-                                <button class = "btn btn-primary">Show Details</button>
+                                <button onclick="showDataDetails('${show._id}')" href= "#" class ="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                                 </div>
+                               
                             </div>
                         </div>
                     </div>
                 `;
         displayDataContainer.appendChild(dataDiv);
     })
+    toggleSpinner(false);
+}
+
+const showDataDetails = async id => {
+    const url = ` https://openapi.programming-hero.com/api/news/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayDataDetails(data.data[0]);
+}
+const displayDataDetails = data => {
+    console.log(data);
+    const modalTitle = document.getElementById('data-title');
+    modalTitle.innerHTML = `
+    <h4>${data.author.name ? data.author.name : 'No Author Name'}</h4>
+    `;
+    const dataDetails = document.getElementById('data-details');
+    dataDetails.innerHTML = `
+    <p>Published date: ${data.author ? data.author.published_date : 'No published date'}</p>
+    <img src="${data.author.img}" class="img-fluid rounded" style="max-width: 50px;" alt="...">
+    `
+}
+
+
+// spinner
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    }
+    else {
+        loaderSection.classList.add('d-none');
+    }
 }
 
 loadNav();
 
-loadData();
+loadData('01');
 
