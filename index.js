25c2fc3bf614d78eb1fb1b74a85b0168010a2db0
hossainahmad1 <108587702+hossainahmad1@users.noreply.github.com>
2022-09-04 08:42:32 +0600

    resolved

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const loadNav = () => {
         .then(data => displayNav(data.data.news_category))
 }
 const displayNav = loads => {
+
     const dataContainer = document.getElementById('data-container');
-
     loads.forEach(load => {
-
+        toggleSpinner(true);
         // console.log(load);
         const createLi = document.createElement('li');
         createLi.innerHTML = `
@@ -16,7 +16,7 @@ const displayNav = loads => {
         `;
         dataContainer.appendChild(createLi);
     });
-    toggleSpinner(true);
+
 }
 
 
@@ -33,15 +33,18 @@ const displayDataLoad = loadData => {
     loadData.forEach(show => {
         console.log(show)
         const dataDiv = document.createElement('div');
-        dataDiv.classList.add('row');
+        dataDiv.classList.add('col-12');
         dataDiv.innerHTML = `
-                    <div class="col-md-4">
-                        <img src="${show.image_url}" class="img-fluid rounded-start" alt="...">
-                    </div>
-                  <div class="col-md-8">
+                <div class="card mb-3" >
+                <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${show.image_url}" class="img-fluid rounded-start" alt="...">
+                 </div>
+                        <div class="col-md-8">
+                    
                         <div class="card-body">
                             <h5 class="card-title">${show.title}</h5>
-                            <p class="card-text text-muted">${show.details}.</p>
+                            <p class="card-text text-muted">${show.details.slice(0, 300) + "..."}.</p>
                             <div class = "d-flex">
                                 <div>
                                 <img src="${show.author.img}" class="img-fluid rounded-circle" style="max-width: 50px;" alt="...">
@@ -56,7 +59,6 @@ const displayDataLoad = loadData => {
                                 </div>
                             </div>
                         </div>
-                    </div>
                 `;
         displayDataContainer.appendChild(dataDiv);
     })
@@ -86,7 +88,7 @@ const displayDataDetails = data => {
 // spinner
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if (isLoading) {
+    if (isLoading === true) {
         loaderSection.classList.remove('d-none');
     }
     else {
